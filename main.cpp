0d553279dd6260da2c4d82c98332eecc65206994
Nate-OBrien <159456288+Nate-OBrien@users.noreply.github.com>
2024-12-18 09:48:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/*
+* Name: Nate O'Brien
+* Date: 12/18
+* Program Name: 12/18 Do Now
+*/
 #include <iostream>
 #include <cmath>
 #include <ctime>
@@ -6,14 +11,18 @@
 using namespace std;
 
 int main() {
+    //gets input
     double x;
     cout << "Enter a double: ";
     cin >> x;
 
+    //prints rounded, with time
     cout << "Rounded value: " << round(x * 100.0)/100.0 << endl;
 
+    //creates random seed
     srand(time(NULL));
 
+    //gemerates rand number
     int secret = rand() % 10 + 1;
 
     cout << "Random Number: " << secret << endl;
